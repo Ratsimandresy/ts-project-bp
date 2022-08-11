3183ed92895128ce65a19843299e83746b1feea6
ratsimandresy <ricorabesoa@gmail.com>
2022-08-11 14:47:09 +0200

    refacto comparer mains

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
+function resultatDuel(m1, m2, c1, c2) {
+  return (m1 == c1 && m2 == c2) || (m2 == c1 && m1 == c2);
+}
+
 function comparerFeuillePierre(m1, m2) {
-  return (m1 == "F" && m2 == "P") || (m2 == "F" && m1 == "P");
+  return resultatDuel(m1, m2, "F", "P");
 }
 
 function comparerPierreCiseaux(m1, m2) {
-  return (m1 == "C" && m2 == "P") || (m2 == "C" && m1 == "P");
+  return resultatDuel(m1, m2, "C", "P");
+}
+
+function comparerFeuilleCiseaux(m1, m2) {
+  return resultatDuel(m1, m2, "F", "C");
 }
 
 export function pfc(m1,m2) {
-  if (m1 === m2) return "=";
-  if (comparerFeuillePierre(m1, m2)) return "F";
-  if (comparerPierreCiseaux(m1, m2)) return "P";
-  return "C";
+  let resultat;
+  if (m1 === m2) resultat = "=";
+  if (comparerFeuillePierre(m1, m2)) resultat = "F";
+  if (comparerPierreCiseaux(m1, m2)) resultat = "P";
+  if (comparerFeuilleCiseaux(m1, m2)) resultat = "C";
+  return resultat;
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,24 +1,25 @@
-import {pfc} from "../src";
+import {definirGagnant, pfc} from "../src";
 
 describe("pierre, feuille, ciseaux", () => {
-  it("doit retourner feuille", () => {
+  it("feuille gagne sur pierre", () => {
     expect(pfc("P", "F")).toEqual("F");
     expect(pfc("F", "P")).toEqual("F");
   });
 
-  it("doit retourner égalité", () => {
+  it("cas égalité, (pierre VS pierre)", () => {
     expect(pfc("P", "P")).toEqual("=");
     expect(pfc("F", "F")).toEqual("=");
     expect(pfc("C", "C")).toEqual("=");
   });
 
-  it('doit retourner pierre',  () => {
+  it('pierre gagne sur ciseaux',  () => {
     expect(pfc("P","C")).toEqual("P");
     expect(pfc("C","P")).toEqual("P");
   });
 
-  it('doit retourner ciseaux',  () => {
+  it('ciseaux gagne sur feuille',  () => {
     expect(pfc("F","C")).toEqual("C");
     expect(pfc("C","F")).toEqual("C");
   });
+
 })
