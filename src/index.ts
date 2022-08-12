f7bfcdb85b065d84251316f096114ce8c5893a7c
ratsimandresy <ricorabesoa@gmail.com>
2022-08-12 16:05:17 +0200

    custom type

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
-function resultatDuel(main1: string, main2 : string, c1 : string, c2:string): boolean{
+type CoupsPossibles = "Pierre" | "Feuille" | "Ciseaux";
+type RetoursPossibles = "Egalite" | CoupsPossibles;
+type Gagnant = "Egalite" | "Joueur 1" | "Joueur 2";
+
+function resultatDuel(main1: CoupsPossibles, main2 : CoupsPossibles, c1 : CoupsPossibles, c2:CoupsPossibles): boolean{
   return (main1 == c1 && main2 == c2) || (main2 == c1 && main1 == c2);
 }
 
-function comparerFeuillePierre(main1 : string, main2 : string): boolean {
+function comparerFeuillePierre(main1 : CoupsPossibles, main2 : CoupsPossibles): boolean {
   return resultatDuel(main1, main2, "Feuille", "Pierre");
 }
 
-function comparerPierreCiseaux(main1 : string, main2 : string) : boolean{
+function comparerPierreCiseaux(main1 : CoupsPossibles, main2 : CoupsPossibles) : boolean{
   return resultatDuel(main1, main2, "Ciseaux", "Pierre");
 }
 
-function comparerFeuilleCiseaux(main1 : string, main2 : string): boolean {
+function comparerFeuilleCiseaux(main1 : CoupsPossibles, main2 : CoupsPossibles): boolean {
   return resultatDuel(main1, main2, "Feuille", "Ciseaux");
 }
 
-export function pfc(main1 : string, main2 : string): string {
+export function pfc(main1 : CoupsPossibles, main2 : CoupsPossibles): RetoursPossibles {
   let resultat;
   if (main1 === main2) resultat = "Egalite";
   if (comparerFeuillePierre(main1, main2)) resultat = "Feuille";
@@ -23,7 +27,7 @@ export function pfc(main1 : string, main2 : string): string {
   return resultat;
 }
 
-function calculScore(main1 : string, main2 : string) : number[] {
+function calculScore(main1 : CoupsPossibles, main2 : CoupsPossibles) : number[] {
   if (definirGagnant(main1, main2) == "Joueur 1") {
     return [1, -1];
   }
@@ -33,7 +37,7 @@ function calculScore(main1 : string, main2 : string) : number[] {
   return [0, 0];
 }
 
-function definirGagnant(main1 : string, main2 : string) : string {
+function definirGagnant(main1 : CoupsPossibles, main2 : CoupsPossibles) : Gagnant {
   let resultatDuel = pfc(main1, main2);
   if (main1 == resultatDuel) return "Joueur 1";
   if (main2 == resultatDuel) return "Joueur 2";
@@ -44,7 +48,8 @@ function ajoutResultatParRound(tableauDesResultats : number[], resultRound : num
   return tableauDesResultats.map((scoreActuel, index) => scoreActuel + resultRound[index])
 }
 
-export function calculScoreTotal(...coupsJoueurs : string[][]) : number[] {
+
+export function calculScoreTotal(...coupsJoueurs : CoupsPossibles[][]) : number[] {
   if (coupsJoueurs[0].length != coupsJoueurs[1].length) throw new Error("Nombres coups différents");
   let tableauDesResultats = Array(coupsJoueurs.length).fill(0);
   coupsJoueurs[0].map((a,b) => {
